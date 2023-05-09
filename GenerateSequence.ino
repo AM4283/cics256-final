de82f0ab2ef22ece178d7fc7e5616cd6a665f567
jngooi03 <120337452+jngooi03@users.noreply.github.com>
2023-05-09 13:25:35 -0400

    Add files via upload

--- GenerateSequence.ino
+++ GenerateSequence.ino
@@ -1,21 +1,40 @@
+#include <Wire.h>
+#include "Adafruit_Trellis.h"
+
+Adafruit_Trellis matrix0 = Adafruit_Trellis();
+Adafruit_TrellisSet trellis =  Adafruit_TrellisSet(&matrix0);
+#define INTPIN 5
+
 const byte buttons[]={34,0,35};
 int SIZE = 10;
 
 int sequence[10];
-int j = 0; // index of sequence that player is on
-int n = 3; // number of Buttons
+int j = 0; // index of sequence that player is on (SCORE)
+int count = 0; // index of sequence that player is currently on
+int n = 16; // number of Buttons
 
 int curStates[3];
 int prevStates[3];
 
 void setup() {
   Serial.begin(9600);
-  for(byte i=0;i<3;i++) {
-    pinMode(buttons[i], INPUT_PULLUP);
+
+  // INT pin requires a pullup
+  pinMode(INTPIN, INPUT);
+  digitalWrite(INTPIN, HIGH);
+  
+  trellis.begin(0x70);
+  trellis.setBrightness(150);
+  for (uint8_t i=0; i<16; i++) {
+    trellis.clrLED(i);
   }
-  for (int i=0; i<n; i++) { // Initialize prevStates values to 1 because buttons default to high
-    prevStates[i] = 1;
-  }
+  trellis.writeDisplay();
+  // for(byte i=0;i<3;i++) {
+  //   pinMode(buttons[i], INPUT_PULLUP);
+  // }
+  // for (int i=0; i<n; i++) { // Initialize prevStates values to 1 because buttons default to high
+  //   prevStates[i] = 1;
+  // }
   generateSequence(SIZE, n);
   printArr();
 }
@@ -34,27 +53,51 @@ void generateSequence(int len, int n) { //Generate a sequence of len numbers [0,
   }
 }
 
+void toggle(int placeVal) {
+ if (trellis.isLED(placeVal))
+    trellis.clrLED(placeVal);
+  else
+    trellis.setLED(placeVal);
+}
+
+int sequenceDone = 0;
 void loop() {
+  delay(800);
+  if (sequenceDone == 0) {
+    for (int i = 0; i<j+1; i++) {
+      toggle(sequence[i]);
+      trellis.writeDisplay();
+      delay(500);
+      toggle(sequence[i]);
+      trellis.writeDisplay();
+      delay(500);
+    }
+    sequenceDone = 1;
+  }
   int isPressed = 0; // Default to False
   int buttonPressed = -1;
-  for (int i = 0; i<3; i++) {
-    curStates[i] = digitalRead(buttons[i]);
+  if (trellis.readSwitches()) {
+    for(int i=0; i<16; i++) {
+      if (trellis.justPressed(i)) {
+        buttonPressed = i;
+        isPressed = 1;
+        break;
+      }
+    }
   }
   
-  for (int i = 0; i<n; i++) {
-    if (!curStates[i] && prevStates[i]) { // button pressed
-      isPressed = 1;
-      buttonPressed = i;
-      break;
-    }
-    prevStates[i] = curStates[i]; // Seems to be not correctly updating prevStates
-
-  } // NEED TO IMPLEMENT BUTTON DEBOUNCING?? OR JUST USE DELAY
-
-  if (isPressed) {
-    if (buttonPressed == sequence[j]) {
-      Serial.println("Correct!");
-      j++;
+  if (isPressed) { 
+    if (buttonPressed == sequence[count]) {
+      if (count == j) {
+        Serial.println("Correct!");
+        j++;
+        count = 0;
+        sequenceDone = 0;
+      }
+      else {
+        Serial.println("keep going");
+        count++;
+      }
     }
     else {
       Serial.println("Wrong!");
@@ -64,8 +107,9 @@ void loop() {
       generateSequence(SIZE, n);
       printArr();
       j=0;
+      count = 0;
+      sequenceDone = 0;
     }
-    delay(500);
   }
 }
 
